c461dab02b29fce1f14aef92e43faf2cf98c46be
mriziq <amerkmriziq@gmail.com>
2023-11-10 03:18:59 -0800

    keywords seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: '2bttns | Personalize your app',
   description: 'Easily capture rich user preference data to accurately personalize content in your web app. 100% Javascript. Integrate with just a few lines of code.',
+  keywords: "Interactive two-button game, Web application user engagement, User preference data collection, Personalized content recommendation, JavaScript SDK integration, RESTful API for web apps, No-code admin panel, CMS functionality enhancement, User experience personalization, Algorithm bias reduction, Customizable user engagement game, Web app data management, Streamlined content curation, Enhanced web app interaction, User data analytics tool"
 }
 
 export default function RootLayout({
