61afa3d0f99d79b7129ef0b259c40852a7bf7da0
mriziq <amerkmriziq@gmail.com>
2024-02-07 01:29:42 -0800

    h4i page

---
+++ app/components/EmailForm/EmailForm.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  VStack,
+  Center
+} from '@chakra-ui/react';
+
+const EmailInputForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+
+  const sendToDiscord = async (emailAddress: string) => {
+    if (emailAddress) {
+      try {
+        const webhookContent = JSON.stringify({
+          content: `New email submission: ${emailAddress}`,
+        });
+
+        await fetch(process.env.NEXT_PUBLIC_TWOBTTNS_DISCORD_WEBHOOK as string, {
+          method: 'POST',
+          body: webhookContent,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        console.log('Email submitted to Discord!');
+      } catch (error) {
+        console.error('Failed to send to Discord.', error);
+      }
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    sendToDiscord(email);
+    setEmail(''); // Clear the input after submission
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Center>
+      <VStack
+        // direction={['column', null, 'row']} // Responsive layout
+        align="center"
+        gap="4"
+      >
+        <FormControl isRequired>
+          <FormLabel htmlFor='email'>Email address</FormLabel>
+          <Input
+            id='email'
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Enter your email'
+            width={"50vw"}
+          />
+        </FormControl>
+        <Button type='submit' colorScheme='green' width={"50vw"}>Gain Access</Button>
+      </VStack>
+      </Center>
+    </form>
+  );
+};
+
+export default EmailInputForm;
---
+++ app/h4i/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+import { Providers } from "../providers";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: '2bttns | Hack for Impact',
+  description: 'Easily capture rich user preference data to accurately personalize content in your web app. 100% Javascript. Integrate with just a few lines of code.',
+  keywords: "Interactive two-button game, Web application user engagement, User preference data collection, Personalized content recommendation, JavaScript SDK integration, RESTful API for web apps, No-code admin panel, CMS functionality enhancement, User experience personalization, Algorithm bias reduction, Customizable user engagement game, Web app data management, Streamlined content curation, Enhanced web app interaction, User data analytics tool"
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
+}
